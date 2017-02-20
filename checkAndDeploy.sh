b994c2232ebc796fd2409aafa8594e801170fc67
Gustavo Sousa <gustavocms@gmail.com>
2017-02-20 17:59:37 +0100

    Updating scripts.

--- checkAndDeploy.sh
+++ checkAndDeploy.sh
@@ -2,42 +2,6 @@
 
 set -x
 
-
-
-check () {
-  GIT_URL="$1"
-  PROJECT="$2"
-  BUILD_DIR="$3"
-  
-  git config --global user.email "$USER@localhost"
-  git config --global user.name "$USER"
-  
-  if [ ! "$GIT_URL" ] || [ ! "$PROJECT" ] || [ ! "$BUILD_DIR" ]; then
-    echo Usage: $0 GIT_URL PROJECT BUILD_DIR
-    exit 1
-  fi
-  
-  
-  if [ ! -d "$BUILD_DIR" ]; then
-    mkdir -p "$BUILD_DIR"
-    cd $BUILD_DIR/..
-    rmdir $BUILD_DIR
-    git clone $GIT_URL
-    cd $BUILD_DIR
-  
-    echo Updated
-    exit 0
-  fi
-  
-  cd $BUILD_DIR
-  GIT_PULL=$(git pull | egrep "[A-Za-z0-9]+\.\.[A-Z0-9a-z]+")
-  if [ "$GIT_PULL" ] && [ "$?" == "0"  ]; then
-    echo Updated: $GIT_PULL
-  fi
-
-}
-
-
 APP_USER=web
 PROJECT=youtube2podcast
 GIT_URL=http://github.com/gcms/$PROJECT.git
@@ -48,7 +12,7 @@ fi
 
 BASEDIR=$(dirname $0)
 
-UPDATED=$(sudo -u $APP_USER check "$GIT_URL" "$PROJECT" "$BUILD_DIR")
+UPDATED=$(sudo -u $APP_USER $BASEDIR/check.sh "$GIT_URL" "$PROJECT" "$BUILD_DIR")
 if [ "$UPDATED" ]; then
   touch "$BUILD_DIR/.build"
 fi
